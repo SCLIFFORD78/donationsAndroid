67251d31fb6783ae4065284ae335bd9dd586c980
SCLIFFORD78 <faugh01@gmail.com>
2021-12-17 01:39:50 +0000

    google sign added

--- app/src/main/java/ie/wit/donationx/firebase/FirebaseAuthManager.kt
+++ app/src/main/java/ie/wit/donationx/firebase/FirebaseAuthManager.kt
@@ -2,13 +2,20 @@ package ie.wit.donationx.firebase
 
 import android.app.Application
 import androidx.lifecycle.MutableLiveData
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import ie.wit.donationx.R
 import timber.log.Timber
 
 class FirebaseAuthManager(application: Application) {
 
     private var application: Application? = null
+    var googleSignInClient = MutableLiveData<GoogleSignInClient>()
 
     var firebaseAuth: FirebaseAuth? = null
     var liveFirebaseUser = MutableLiveData<FirebaseUser>()
@@ -24,6 +31,7 @@ class FirebaseAuthManager(application: Application) {
             loggedOut.postValue(false)
             errorStatus.postValue(false)
         }
+        configureGoogleSignIn()
     }
 
     fun login(email: String?, password: String?) {
@@ -39,6 +47,16 @@ class FirebaseAuthManager(application: Application) {
             })
     }
 
+    private fun configureGoogleSignIn() {
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(application!!.getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient.value = GoogleSignIn.getClient(application!!.applicationContext,gso)
+    }
+
     fun register(email: String?, password: String?) {
         firebaseAuth!!.createUserWithEmailAndPassword(email!!, password!!)
             .addOnCompleteListener(application!!.mainExecutor, { task ->
@@ -50,6 +68,25 @@ class FirebaseAuthManager(application: Application) {
                     errorStatus.postValue(true)
                 }
             })
+    }
+
+    fun firebaseAuthWithGoogle(acct: GoogleSignInAccount) {
+        Timber.i( "DonationX firebaseAuthWithGoogle:" + acct.id!!)
+
+        val credential = GoogleAuthProvider.getCredential(acct.idToken, null)
+        firebaseAuth!!.signInWithCredential(credential)
+            .addOnCompleteListener(application!!.mainExecutor) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update with the signed-in user's information
+                    Timber.i( "signInWithCredential:success")
+                    liveFirebaseUser.postValue(firebaseAuth!!.currentUser)
+
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Timber.i( "signInWithCredential:failure $task.exception")
+                    errorStatus.postValue(true)
+                }
+            }
     }
 
     fun logOut() {
